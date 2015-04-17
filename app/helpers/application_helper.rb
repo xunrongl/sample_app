b3f2e8efb2c43cf1cb41d8c904f238619527ecc5
xunrongl <lixunrong@gmail.com>
2015-04-17 16:00:24 -0400

    add helper to title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# return the complete title
+	def full_title(page_title = '')
+		base_title = "ROR Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "ROR Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "Help | ROR Tutorial Sample App"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "About | ROR Tutorial Sample App"
   end
 end
