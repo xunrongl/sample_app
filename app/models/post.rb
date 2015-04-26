535eb88bb0e71eefff2a32a35e85dbfcf642d467
xunrongl <lixunrong@gmail.com>
2015-04-26 13:00:32 -0400

    post test module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :content, presence: true
+  validates :longtitude, presence: true
+  validates :latitude, presence: true
+  validates :imei, presence: true
+  validates :like, presence: true
+  validates :report, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,8 +4,22 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-	def method_name
-		@post = Post.new(content: "example content", latitude: "0.0", longtitute: "0.0", imei: "00000", )	
-	end
+  def setup
+	@post = Post.new(content: "example content", latitude: "0.0", longtitude: "0.0", imei: "00000", like: "0", report: "0")
+  end
 
+  test "should be valid" do
+  	assert @post.valid?
+  end
+
+  test "content, postion and imei should be present" do
+  	@post.content = "	"
+  	assert_not @post.valid?
+  end
+
+  test "content should be shorter than 140 characters" do
+  	@post.content = "a" * 141
+  	assert_not @post.valid?
+  end
+  
 end
